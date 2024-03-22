24ccfec98d7ac0c2f48803b0fe4eb8db7648d582
treamy1 <treamy@msudenver.edu>
2024-03-22 15:46:43 -0600

    edit login_test to add test user to session

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,5 +1,5 @@
 import unittest
-from app import app, bcrypt
+from app import app, bcrypt, db
 from app.models import User
 
 class LoginTestCase(unittest.TestCase):
@@ -10,8 +10,9 @@ class LoginTestCase(unittest.TestCase):
 
         # create a test user
         self.user = User(id='test', name='Test User', passwd=bcrypt.hashpw('test'.encode('utf-8'), bcrypt.gensalt()))
-        self.user.save() # save the user to the database
-
+        db.session.add(self.user) # add the test user to the database
+        db.session.commit() # commit the changes
+        
     def tearDown(self):
         self.user.delete() # remove the test user from the database
 
@@ -20,7 +21,7 @@ class LoginTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Logged in successfully', response.data) # check if 'Welcome' message is present in the response
 
-    def test_incorrct_login(self):
+    def test_incorrect_login(self):
         response = self.app.post('/users/login', data=dict(id='test', passwd='wrong'), follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Invalid ID or password', response.data) # check if 'Invalid credentials' message is present
